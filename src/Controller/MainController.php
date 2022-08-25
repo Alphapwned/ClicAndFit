a8a2078cda7560a7a6f0107d43f6bd08edf420b1
Alphapwned <fondevila.gautier@gmail.com>
2022-08-25 15:43:48 +0200

    mise en prod

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,14 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AnnoncesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(AnnoncesRepository $annoncesRepository): Response
+    public function index(Request $request, AnnoncesRepository $annoncesRepository, PaginatorInterface $paginator): Response
     {
+
+        $annonces = $annoncesRepository->findAll();
+
+        $annonces = $paginator->paginate(
+            $annonces, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            5/*limit per page*/
+        );
+
         return $this->render('main/index.html.twig', [
-            'annonces' => $annoncesRepository->findAll(),
+            'annonces' => $annonces,
         ]);
     }
 }
